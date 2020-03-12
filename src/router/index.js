578ed2fb030cfc557c11c43ee1902b1a858c966f
qingyun_fan <qingyun_fan@rainbowtech.net.cn>
2020-03-12 15:22:53 +0800

    测试路由的children字段

--- src/router/index.js
+++ src/router/index.js
@@ -11,17 +11,27 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
+    path: '',
     name: 'About',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    children: [
+      {
+        path: '/child',
+        name: 'child',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "about" */ '../views/Child.vue'),
+      }
+    ]
   }
 ]
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: 'hash',
   base: process.env.BASE_URL,
   routes
 })
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <!-- 这里渲染子组件 -->
+    <router-view/>
   </div>
 </template>
